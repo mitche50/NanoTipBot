d5a99d6161d7eb133bba1440f50ec17f99c1d2ea
Andrew Mitchell <mitche50@gmail.com>
2020-03-09 21:44:26 -0400

    add user from address

--- webhooks.py
+++ webhooks.py
@@ -351,8 +351,8 @@ def get_user_from_address(address):
     address_values = [address,]
     address_return = modules.db.get_db_data_new(address_call, address_values)
     try:
-        if address_return[0][0] is not None:    
-            return address_return[0][0]
+        if address_return[0] is not None:    
+            return address_return[0]
         else:
             return "There is no user tied to address {}".format(address)
     except Exception as e:
