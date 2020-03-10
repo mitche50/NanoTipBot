a3b765c2c52fec5c93d20f716ffb78c0bec12b48
Andrew Mitchell <mitche50@gmail.com>
2020-03-09 22:45:32 -0400

    CORS fix

--- webhooks.py
+++ webhooks.py
@@ -402,7 +402,7 @@ def get_all_users_telegram():
                     "user_name": user[1],
                     "account": user[2]
                 })
-            response = flask.jsonify(json_response)
+            response = jsonify(json_response)
             response.headers.add('Access-Control-Allow-Origin', '*')
             return response, 200
         else:
